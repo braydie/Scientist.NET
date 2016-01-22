a9ddab066ac8e88141b884ea3b39e6f8b7bc824a
braydie <braydie.grove@gmail.com>
2016-01-22 12:12:01 +0000

    Moved code from Experiment into BaseExperiment to make it a more useful

--- src/Scientist/BaseExperiment.cs
+++ src/Scientist/BaseExperiment.cs
@@ -1,23 +1,90 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Scientist.Exceptions;
 
 namespace Scientist
 {
     public abstract class BaseExperiment
     {
-        internal BaseExperiment()
-        {
-        }
+        private bool _RunIf;
 
         internal BaseExperiment(string NameIn)
         {
             Name = NameIn;
             Publishers = new List<IResultPublisher>();
+            _RunIf = true;
         }
 
         public string Name { get; set; }
         public int PercentageEnabled { get; set; }
-        public List<IResultPublisher> Publishers { get; set; } 
-        
+        public List<IResultPublisher> Publishers { get; set; }
+        internal dynamic Control { get; set; }
+        internal dynamic Candidate { get; set; }
+
+        public void RunIf(bool RunIf)
+        {
+            _RunIf = RunIf;
+        }
+
+        public dynamic Run()
+        {
+            if (Control == null)
+            {
+                throw new BehaviourMissingException(this, "Control");
+            }
+            var Result = new Result(this);
+            Observation CandidateObservation;
+            Observation ControlObservation;
+            var Rand = new Random();
+            if(Rand.Next(0,2) == 0)
+            {
+                ControlObservation = GetControlObservation();
+                Result.Observations.Add(ControlObservation);
+                CandidateObservation = GetCandidateObservation();
+                if (CandidateObservation != null)
+                {
+                    Result.Observations.Add(CandidateObservation);
+                }
+            }
+            else
+            {
+                CandidateObservation = GetCandidateObservation();
+                if (CandidateObservation != null)
+                {
+                    Result.Observations.Add(CandidateObservation);
+                }
+                ControlObservation = GetControlObservation();
+                Result.Observations.Add(ControlObservation);
+            }
+
+            Publish(Result);
+            return ControlObservation.Result;
+        }
+
+        private Observation GetCandidateObservation()
+        {
+            Observation CandidateObservation = null;
+            if (ExperimentShouldRun())
+            {
+                if (_RunIf)
+                {
+                    CandidateObservation = new Observation(this, "Candidate", Candidate);                    
+                }
+            }
+            return CandidateObservation;            
+        }
+
+        private Observation GetControlObservation()
+        {
+            return new Observation(this, "Control", Control);
+        }
+
+        private bool ExperimentShouldRun()
+        {
+            var Rand = new Random();
+            return PercentageEnabled > 0 && Rand.Next(100) < PercentageEnabled;
+        }
+
         internal void Publish(Result Result)
         {
             foreach (var ResultPublisher in Publishers)
--- src/Scientist/Experiment.cs
+++ src/Scientist/Experiment.cs
@@ -4,11 +4,7 @@ using Scientist.Exceptions;
 namespace Scientist
 {
     public class Experiment<T> : BaseExperiment
-    {
-        private dynamic _Control;
-        private dynamic _Candidate;
-        private bool _RunIf;
-
+    {                     
         public Experiment() 
             : this("Experiment")
         {            
@@ -16,86 +12,26 @@ namespace Scientist
 
         public Experiment(string ExperimentName) 
             : base(ExperimentName)
-        {            
-            _RunIf = true;
+        {                        
         }
 
-        public void Use(Func<T> Control)
+        public void Use(Func<T> ControlIn)
         {
-            if(_Control != null)
+            if(Control != null)
             {
                 throw new BehaviourNotUniqueException(this, "Control");
             }
-            _Control = Control;
+            Control = ControlIn;
         }
         
-        public void Try(Func<T> Candidate)
+        public void Try(Func<T> CandidateIn)
         {
-            _Candidate = Candidate;
+            Candidate = CandidateIn;
         }
-
-        public void RunIf(bool RunIf)
+        
+        public new T Run()
         {
-            _RunIf = RunIf;
-        }
-
-        public T Run()
-        {            
-            if (_Control == null)
-            {
-                throw new BehaviourMissingException(this, "Control");
-            }
-            var Result = new Result(this);
-            Observation Candidate;
-            Observation Control;
-            var Rand = new Random();
-            if(Rand.Next(0,2) == 0)
-            {
-                Control = ObserveControl();
-                Result.Observations.Add(Control);
-                Candidate = ObserveCandidate();
-                if (Candidate != null)
-                {
-                    Result.Observations.Add(Candidate);
-                }
-            }
-            else
-            {
-                Candidate = ObserveCandidate();
-                if (Candidate != null)
-                {
-                    Result.Observations.Add(Candidate);
-                }
-                Control = ObserveControl();
-                Result.Observations.Add(Control);
-            }
-
-            Publish(Result);
-            return (T)Control.Result;
-        }
-
-        private Observation ObserveCandidate()
-        {
-            Observation Candidate = null;
-            if (ExperimentShouldRun())
-            {
-                if (_RunIf)
-                {
-                    Candidate = new Observation(this, "Candidate", _Candidate);                    
-                }
-            }
-            return Candidate;            
-        }
-
-        private Observation ObserveControl()
-        {
-            return new Observation(this, "Control", _Control);
-        }
-
-        private bool ExperimentShouldRun()
-        {
-            var Rand = new Random();
-            return PercentageEnabled > 0 && Rand.Next(100) < PercentageEnabled;
+            return (T)base.Run();
         }
     }    
 }
